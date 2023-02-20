da479b7632a508d7e6009c6d9657935737dd59db
haileypak <haileypak.me@gmail.com>
2023-02-21 08:40:11 +0900

    섹션 11. [프로젝트] [★★☆☆☆] 쓸데없이 이쁜 랜덤숫자 생성기

---
+++ code factory/level1/sect11_project_random_number_generator/android/app/src/main/kotlin/com/example/sect11_project_random_number_generator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sect11_project_random_number_generator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ code factory/level1/sect11_project_random_number_generator/lib/component/number_row.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NumberRow extends StatelessWidget {
+  final int number;
+
+  const NumberRow({
+    required this.number,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: number
+          .toString()
+          .split('')
+          .map(
+            (x) => Image.asset(
+              'asset/img/$x.png',
+              width: 50.0,
+              height: 70.0,
+            ),
+          )
+          .toList(),
+    );
+  }
+}
