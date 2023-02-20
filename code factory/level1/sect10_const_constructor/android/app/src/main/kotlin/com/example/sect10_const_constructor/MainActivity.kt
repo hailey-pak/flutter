06a5c7ee2cbcebdc1ffdec53a5464c846f2f8732
haileypak <haileypak.me@gmail.com>
2023-02-20 14:05:57 +0900

    섹션 10. [이론] Const Constructor

---
+++ code factory/level1/sect10_const_constructor/android/app/src/main/kotlin/com/example/sect10_const_constructor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sect10_const_constructor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ code factory/level1/sect10_const_constructor/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:sect10_const_constructor/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
