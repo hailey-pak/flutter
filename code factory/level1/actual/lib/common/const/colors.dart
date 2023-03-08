d11b56e648f70113a4b8ae9bf96f08767f03b356
haileypak <haileypak.me@gmail.com>
2023-03-08 15:40:19 +0900

    섹션 1. 기본 UI 작업하기

---
+++ code factory/level1/actual/lib/common/const/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const PRIMARY_COLOR = Color(0xff22A45D);     //주 색상
+const BODY_TEXT_COLOR = Color(0xff868686);    //글자 색상
+const INPUT_BG_COLOR = Color(0xffFBFBFB);     //텍스트 필드 배경 색상
+const INPUT_BORDER_COLOR = Color(0xffF3F2F2); //텍스트 필드 테두리 색상
