53348fe1fcd8fd17e1d142e3544c1c3dadfcb988
haileypak <haileypak.me@gmail.com>
2023-03-16 16:59:56 +0900

    섹션 7. Riverpod을 이용한 상태관리 - Family Modifier 실습

---
+++ code factory/level2/riverpod_theory/lib/screen/family_modifier_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_theory/layout/default_layout.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_theory/riverpod/faimly_modifier_provider.dart';
+
+class FamilyModifierScreen extends ConsumerWidget {
+  const FamilyModifierScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state = ref.watch(familyModifierProvider(5));
+
+    return DefaultLayout(
+      title: 'FamilyModifierScreen',
+      body: Center(
+        child: state.when(
+          data: (data) => Text(data.toString()),
+          error: (error, stackTrace) => Text(error.toString()),
+          loading: () => CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
--- code factory/level2/riverpod_theory/lib/screen/home_scree.dart
+++ code factory/level2/riverpod_theory/lib/screen/home_scree.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_theory/layout/default_layout.dart';
+import 'package:riverpod_theory/screen/family_modifier_screen.dart';
 import 'package:riverpod_theory/screen/future_provider_screen.dart';
 import 'package:riverpod_theory/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_theory/screen/state_provider_screen.dart';
@@ -53,6 +54,16 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: Text('StreamProviderScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => FamilyModifierScreen(),
+                  ),
+                );
+              },
+              child: Text('FamilyModifierScreen'),
             ),
           ],
         ));
