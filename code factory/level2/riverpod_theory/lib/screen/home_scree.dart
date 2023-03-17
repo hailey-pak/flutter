b69979332efeb09c0433c8f5ebf065306f0c0d36
haileypak <haileypak.me@gmail.com>
2023-03-17 16:24:21 +0900

    섹션 7. Riverpod을 이용한 상태관리 - AutoDispose Modifier 실습

--- code factory/level2/riverpod_theory/lib/screen/home_scree.dart
+++ code factory/level2/riverpod_theory/lib/screen/home_scree.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_theory/layout/default_layout.dart';
+import 'package:riverpod_theory/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_theory/screen/family_modifier_screen.dart';
 import 'package:riverpod_theory/screen/future_provider_screen.dart';
 import 'package:riverpod_theory/screen/state_notifier_provider_screen.dart';
@@ -64,6 +65,16 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: Text('FamilyModifierScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => AutoDisposeModifierScreen(),
+                  ),
+                );
+              },
+              child: Text('AutoDisposeModifierScreen'),
             ),
           ],
         ));
