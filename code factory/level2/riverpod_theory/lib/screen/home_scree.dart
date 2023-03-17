596c83dae3d2b7723ddf837241af339adb500b1e
haileypak <haileypak.me@gmail.com>
2023-03-17 16:42:49 +0900

    섹션 7. Riverpod을 이용한 상태관리 - Listen 함수 실습

--- code factory/level2/riverpod_theory/lib/screen/home_scree.dart
+++ code factory/level2/riverpod_theory/lib/screen/home_scree.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_theory/layout/default_layout.dart';
 import 'package:riverpod_theory/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_theory/screen/family_modifier_screen.dart';
 import 'package:riverpod_theory/screen/future_provider_screen.dart';
+import 'package:riverpod_theory/screen/listen_provider_screen.dart';
 import 'package:riverpod_theory/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_theory/screen/state_provider_screen.dart';
 import 'package:riverpod_theory/screen/stream_provider_screen.dart';
@@ -75,6 +76,16 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: Text('AutoDisposeModifierScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => ListenProviderScreen(),
+                  ),
+                );
+              },
+              child: Text('ListenProviderScreen'),
             ),
           ],
         ));
