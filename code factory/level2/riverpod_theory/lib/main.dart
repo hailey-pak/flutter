69b8c267494de6bc0094b75ff3223d06df58de0b
haiely-pak <haileypak.me@gmail.com>
2023-03-19 22:49:41 +0900

    섹션 7. Riverpod을 이용한 상태관리 -> ProviderObserver 실습

--- code factory/level2/riverpod_theory/lib/main.dart
+++ code factory/level2/riverpod_theory/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_theory/riverpod/provider_observer.dart';
 import 'package:riverpod_theory/screen/home_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     ProviderScope(
-     child: MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: HomeScreen(),
-     ),
+      observers: [
+        Logger(),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomeScreen(),
+      ),
     ),
   );
 }
-
--- code factory/level2/riverpod_theory/lib/screen/home_screen.dart
+++ code factory/level2/riverpod_theory/lib/screen/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:riverpod_theory/layout/default_layout.dart';
 import 'package:riverpod_theory/screen/auto_dispose_modifier_screen.dart';
+import 'package:riverpod_theory/screen/code_generation_screen.dart';
 import 'package:riverpod_theory/screen/family_modifier_screen.dart';
 import 'package:riverpod_theory/screen/future_provider_screen.dart';
 import 'package:riverpod_theory/screen/listen_provider_screen.dart';
@@ -108,6 +109,16 @@ class HomeScreen extends StatelessWidget {
                 );
               },
               child: Text('ProviderScreen'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (_) => CodeGenerationScreen(),
+                  ),
+                );
+              },
+              child: Text('CodeGenerationScreen'),
             ),
           ],
         ));
