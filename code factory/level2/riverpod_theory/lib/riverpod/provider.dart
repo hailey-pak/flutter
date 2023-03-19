efbad0561ff7aa52be61f63a511fd10b39db145e
haiely-pak <haileypak.me@gmail.com>
2023-03-19 22:12:23 +0900

    섹션 7. Riverpod을 이용한 상태관리 -> Provider안에 Provider 사용하기

---
+++ code factory/level2/riverpod_theory/lib/riverpod/provider.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_theory/riverpod/state_notifier_provider.dart';
+
+final filteredShoppingListProvider = Provider(
+  (ref) {
+    final filterState = ref.watch(filterProvider);
+    final shoppingListState = ref.watch(shoppingListProvider);
+
+    if (filterState == FilterState.all) {
+      return shoppingListState;
+    }
+
+    return shoppingListState
+        .where((element) => filterState == FilterState.spicy
+            ? element.isSpicy
+            : !element.isSpicy)
+        .toList();
+  },
+);
+
+enum FilterState {
+  notSpicy,
+  spicy,
+  all,
+}
+
+final filterProvider = StateProvider<FilterState>((ref) => FilterState.all);
