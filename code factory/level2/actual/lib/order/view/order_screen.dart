edb5b45aaf3b2933da0ae368f12dc0cac848ba19
haileypak <haileypak.me@gmail.com>
2023-03-24 17:04:46 +0900

    섹션 17. 주문 탭

---
+++ code factory/level2/actual/lib/order/view/order_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class OrderScreen extends ConsumerWidget {
+  const OrderScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Center(
+      child: Text('주문'),
+    );
+  }
+}
