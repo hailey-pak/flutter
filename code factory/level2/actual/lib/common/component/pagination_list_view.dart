f2bbdd6e64f9b243bc2174c41a2513edc15ab713
haiely-pak <haileypak.me@gmail.com>
2023-03-26 22:26:59 +0900

    섹션 17. 주문 탭 완료

--- code factory/level2/actual/lib/common/component/pagination_list_view.dart
+++ code factory/level2/actual/lib/common/component/pagination_list_view.dart
@@ -96,35 +96,43 @@ class _PaginationListViewState<T extends IModelWithId>
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: ListView.separated(
-        controller: controller,
-        itemCount: cp.data.length + 1,
-        itemBuilder: (context, index) {
-          if (index == cp.data.length) {
-            return Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
-              child: Center(
-                child: cp is CursorPaginationFetchingMore
-                    ? CircularProgressIndicator()
-                    : Text('마지막 데이터입니다 ㅠㅠ'),
-              ),
-            );
-          }
-
-          final pItem = cp.data[index];
-
-          return widget.itemBuilder(
-            context,
-            index,
-            pItem,
+      child: RefreshIndicator(
+        onRefresh: () async {
+          ref.read(widget.provider.notifier).paginate(
+            forceRefetch: true,
           );
         },
-        separatorBuilder: (context, index) {
-          return SizedBox(height: 16.0);
-        },
+        child: ListView.separated(
+          physics: AlwaysScrollableScrollPhysics(),
+          controller: controller,
+          itemCount: cp.data.length + 1,
+          itemBuilder: (context, index) {
+            if (index == cp.data.length) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 16.0,
+                  vertical: 8.0,
+                ),
+                child: Center(
+                  child: cp is CursorPaginationFetchingMore
+                      ? CircularProgressIndicator()
+                      : Text('마지막 데이터입니다 ㅠㅠ'),
+                ),
+              );
+            }
+
+            final pItem = cp.data[index];
+
+            return widget.itemBuilder(
+              context,
+              index,
+              pItem,
+            );
+          },
+          separatorBuilder: (context, index) {
+            return SizedBox(height: 16.0);
+          },
+        ),
       ),
     );
   }
--- code factory/level2/actual/lib/order/component/order_card.dart
+++ code factory/level2/actual/lib/order/component/order_card.dart
@@ -42,11 +42,13 @@ class OrderCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
           '${orderDate.year}.${orderDate.month.toString().padLeft(2, '0')}.${orderDate.day.toString().padLeft(2, '0')} 주문 완료',
           style: TextStyle(),
         ),
+        const SizedBox(height: 8.0),
         Row(
           children: [
             ClipRRect(
@@ -55,6 +57,7 @@ class OrderCard extends StatelessWidget {
             ),
             const SizedBox(width: 16.0),
             Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   name,
