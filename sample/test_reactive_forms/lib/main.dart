a1d228901eb73a1b7e413c8a66636b5f8c4aea83
haileypak <haileypak.me@gmail.com>
2023-04-03 14:31:13 +0900

    reactive_forms 적용

---
+++ sample/test_reactive_forms/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:test_reactive_forms/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
