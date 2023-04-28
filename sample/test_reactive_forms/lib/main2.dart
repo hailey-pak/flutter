f0e6df2ca5a33e792af80fa495e0fe1b93f0373b
haileypak <haileypak.me@gmail.com>
2023-04-28 17:03:42 +0900

    수정수정

--- sample/test_reactive_forms/lib/main2.dart
+++ sample/test_reactive_forms/lib/main2.dart
@@ -1,18 +1,93 @@
-import 'package:flutter/material.dart';
-import 'package:test_reactive_forms/screen/home_screen_2.dart';
-import 'package:test_reactive_forms/test/basic_data.dart';
+import 'dart:io';
 
+import 'package:flutter/material.dart';
+import 'package:reactive_image_picker/reactive_image_picker.dart';
+import 'package:reactive_multi_image_picker/reactive_multi_image_picker.dart';
+import 'package:test_reactive_forms/screen/home_screen_2.dart';
 
 void main() async {
-
-  BasicData
-
-
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen2(),
+      home: MyHomePage(),
     ),
   );
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Image> imageList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('test'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            //새로이 추가된 부분!!! 사진을 화면에 그려주기 위한 부분이다.
+            imageList.isEmpty
+                ? Container()
+                : SizedBox(
+                    height: 400,
+                    width: MediaQuery.of(context).size.width,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: imageList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return imageList[index];
+                      },
+                    ),
+                  ),
+            OutlinedButton(
+              child: Container(
+                alignment: Alignment.center,
+                height: 30,
+                width: 250,
+                child: Text(
+                  '갤러리',
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              onPressed: () {
+                getImage();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  getImage() async {
+    List<Image> resultList = [];
+
+    List<XFile> images = await ImagePicker().pickMultiImage();
+
+    resultList = images
+        .map(
+          (xFile) => Image.file(
+            File(xFile.path),
+            width: 300,
+            height: 300,
+          ),
+        )
+        .toList();
+    setState(() {
+      imageList = resultList;
+    });
+  }
+
+  Future<Image> xFileToImage(XFile xFile) async {
+    final bytes = await xFile.readAsBytes();
+    return Image.memory(bytes);
+  }
+}
--- sample/test_reactive_forms/lib/screen/home_screen_2.dart
+++ sample/test_reactive_forms/lib/screen/home_screen_2.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:reactive_forms/reactive_forms.dart';
 import 'package:reactive_image_picker/image_file.dart';
 import 'package:reactive_image_picker/reactive_image_picker.dart';
 import 'package:reactive_multi_image_picker/multi_image.dart';
 import 'package:reactive_multi_image_picker/reactive_multi_image_picker.dart';
 import 'package:test_reactive_forms/layout/default_layout.dart';
-import 'package:test_reactive_forms/main.dart';
 
 class HomeScreen2 extends StatefulWidget {
   const HomeScreen2({Key? key}) : super(key: key);
@@ -18,7 +16,6 @@ class HomeScreen2 extends StatefulWidget {
 class _HomeScreen2State extends State<HomeScreen2> {
   @override
   Widget build(BuildContext context) {
-
     return DefaultLayout(
       title: 'Home Screen',
       body: SafeArea(
@@ -46,6 +43,21 @@ class _MediaFormState extends State<MediaForm> {
     'multiImage': FormControl<MultiImage<String>>(),
   });
 
+  List<Asset> imageList = [];
+
+  getImage() async {
+    List<Asset> resultList = [];
+    resultList =
+    await MultiImagePicker.pickImages(
+        maxImages: 10,
+        enableCamera: true,
+        selectedAssets: imageList,
+    );
+    setState(() {
+      imageList = resultList;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ReactiveForm(
@@ -58,35 +70,68 @@ class _MediaFormState extends State<MediaForm> {
               labelText: '사진 추가',
             ),
           ),
-          // ReactiveMultiImagePicker<String>(
-          //   formControlName: 'multiImage',
-          //   imagePickerBuilder: (pickImage, images, onChange) {
-          //     return Column(
-          //       children: [
-          //         Expanded(
-          //           child: GridView.count(
-          //             crossAxisCount: 3,
-          //             children: renderImageAsset(images.assets),
-          //           ),
-          //         ),
-          //         ElevatedButton(
-          //           onPressed: pickImage,
-          //           child: const Text("Pick images"),
-          //         ),
-          //       ],
-          //     );
-          //   },
-          // ),
+          ReactiveMultiImagePicker<String>(
+            formControlName: 'multiImage',
+            decoration: const InputDecoration(
+              labelText: '사진 추가',
+            ),
+            enableCamera: true,
+            maxImages: 10,
+            imagePickerBuilder: (pickImage, images, onChange) {
+              return Column(
+                children: [
+                  images.images.isEmpty
+                      ? Container()
+                      : SizedBox(
+                          height: MediaQuery.of(context).size.height / 5,
+                          width: MediaQuery.of(context).size.width,
+                          child: ListView.builder(
+                            itemBuilder: (context, index) {
+                              Asset asset = images.assets[index];
+                              return AssetThumb(
+                                asset: asset,
+                                width: 300,
+                                height: 300,
+                              );
+                            },
+                          ),
+                        ),
+                  ElevatedButton(
+                    onPressed: pickImage,
+                    child: const Text("Pick images"),
+                  ),
+                ],
+              );
+            },
+          ),
           ReactiveFormConsumer(
             builder: (context, formGroup, child) {
               return ElevatedButton(
                   child: Text('저장'), onPressed: () => print(form.value));
             },
           ),
+          OutlinedButton(
+            onPressed: () { getImage(); },
+            child: Text('갤러리'),
+          ),
+          imageList.isEmpty ? Container() :
+              Container(
+                height: 400,
+                width: MediaQuery.of(context).size.width,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: imageList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      Asset asset = imageList[index];
+                      return AssetThumb(
+                          asset: asset, width: 300, height: 300);
+                    }),
+              ),
         ],
       ),
     );
   }
+
 
   List<AssetThumb> renderImageAsset(List<Asset> assets) {
     return List.generate(assets.length, (index) {
