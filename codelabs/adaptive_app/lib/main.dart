65b1fbe5d673d045ba692cef94b4421624236207
haileypak <haileypak.me@gmail.com>
2024-02-23 11:03:30 +0900

    Codelabs: Adaptive Apps in Flutter

---
+++ codelabs/adaptive_app/lib/main.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:adaptive_app/src/adaptive_login.dart';
+import 'package:adaptive_app/src/adaptive_playlists.dart';
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:googleapis_auth/auth.dart';
+import 'package:provider/provider.dart';
+
+import 'src/app_state.dart';
+import 'src/playlist_details.dart';
+import 'src/playlists.dart';
+
+// From https://developers.google.com/youtube/v3/guides/auth/installed-apps#identify-access-scopes
+final scopes = [
+  'https://www.googleapis.com/auth/youtube.readonly',
+];
+
+// TODO: Replace with your Client ID and Client Secret for Desktop configuration
+final clientId = ClientId(
+  'TODO-Client-ID.apps.googleusercontent.com',
+  'TODO-Client-secret',
+);
+
+final _router = GoRouter(
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (context, state) {
+        return const AdaptivePlaylists();
+      },
+      // Add redirect configuration
+      redirect: (context, state) {
+        if (!context.read<AuthedUserPlaylists>().isLoggedIn) {
+          return '/login';
+        } else {
+          return null;
+        }
+      },
+      routes: <RouteBase>[
+        // Add new login Route
+        GoRoute(
+          path: 'login',
+          builder: (context, state) {
+            return AdaptiveLogin(
+              clientId: clientId,
+              scopes: scopes,
+              loginButtonChild: const Text('Login to YouTube'),
+            );
+          },
+        ),
+        GoRoute(
+          path: 'playlist/:id',
+          builder: (context, state) {
+            final title = state.uri.queryParameters['title']!;
+            final id = state.pathParameters['id']!;
+            return Scaffold(
+              appBar: AppBar(title: Text(title)),
+              body: PlaylistDetails(
+                playlistId: id,
+                playlistName: title,
+              ),
+            );
+          },
+        ),
+      ],
+    ),
+  ],
+);
+
+void main() {
+  runApp(ChangeNotifierProvider<AuthedUserPlaylists>(
+    create: (context) => AuthedUserPlaylists(),
+    child: const PlaylistsApp(),
+  ));
+}
+
+class PlaylistsApp extends StatelessWidget {
+  const PlaylistsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Your Playlists',
+      theme: FlexColorScheme.light(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      darkTheme: FlexColorScheme.dark(
+        scheme: FlexScheme.red,
+        useMaterial3: true,
+      ).toTheme,
+      themeMode: ThemeMode.dark, // Or ThemeMode.System if you'd prefer
+      debugShowCheckedModeBanner: false,
+      routerConfig: _router,
+    );
+  }
+}
